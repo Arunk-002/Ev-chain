79b1e5f31c79f49da0722fa80cb34d52e56ff175
arun <arunkumarjack7892gmail.com>
2023-10-25 12:38:15 +0530

    project app charging station created. templates added and base page rendered

--- evchain/evchain/settings.py
+++ evchain/evchain/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'charging_station.apps.ChargingStationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
